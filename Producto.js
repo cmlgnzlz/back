a51a56a01f4ca3e30892224a684e47240dff24d5
cmlgnzlz <milogonzm@gmail.com>
2022-08-05 20:02:30 -0400

    Entrega 4

--- Producto.js
+++ Producto.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const express = require("express");
 const { Router } = express;
 const app = express();
-const router = Router();
 const PORT = process.env.PORT || 8080;
+const router = Router();
 const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
 });
@@ -12,7 +12,7 @@ server.on("error", (error)=>console.log(`Error en el servidor ${error}`))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/productos", router)
+app.use("/api/productos", router);
 app.use(express.static('public'));
 
 
@@ -38,36 +38,80 @@ class Contenedor{
         try {
             let datos = await fs.promises.readFile(this.nombreArchivo,"utf-8");
             let datosJson = await JSON.parse(datos);
-            if(datosJson.find(i=>i.id === id)){
-                console.log(datosJson.find(i=>i.id === id));
-                this.byId = datosJson.find(i=>i.id === id);
+            if(datosJson.find(i=>i.id == id)){
+                this.byId = datosJson.find(i=>i.id == id);
             } else{
-                this.byId = null;
-                console.log("No existe elemento con esa ID");
+                this.byId = this.datos = { error : 'producto no encontrado' };
             }
                 
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
 
     async save(product) {
         try {
-            console.log('el producto es',product)
-            let datos = await fs.promises.readFile(this.nombreArchivo,"utf-8")
-            let datosJson = await JSON.parse(datos)
+            let datos = await fs.promises.readFile(this.nombreArchivo,"utf-8");
+            let datosJson = await JSON.parse(datos);
             if (datosJson.length>0){
-                product.id= datosJson.length+1
-                console.log(product)
-                datosJson.push(product)
-                this.datos = datosJson
-                console.log("Producto agregado",this.datos)
+                console.log(datosJson)
+                let maxId = datosJson.map(i=>i.id).sort((a, b) => {if(a == b) {return 0;}if(a < b) {return -1;}return 1;}).splice(-1);
+                let nuevoId = parseInt(maxId)+1;
+                product = {id:nuevoId, ...product}
+                datosJson.push(product);
+                this.byId = product;
+                this.datos = datosJson;
+                fs.promises.writeFile(this.nombreArchivo, JSON.stringify(datosJson, null, 1));
             }
             else{
-                console.log("Vacio")
+                this.datos = { error : 'producto no encontrado' };
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async updateById(id,body) {
+        try {
+            console.log(id,body)
+            let datos = await fs.promises.readFile(this.nombreArchivo,"utf-8");
+            let datosJson = await JSON.parse(datos);
+            if (datosJson.find(i=>i.id == id)){
+                let borrarId = datosJson.find(i=>i.id == id);
+                let posicionId = datosJson.indexOf(borrarId);
+                datosJson.splice(posicionId,1);
+                body = {id:parseInt(id), ...body};
+                datosJson.push(body);
+                this.byId = datosJson;
+                fs.promises.writeFile(this.nombreArchivo, JSON.stringify(datosJson, null, 1));
+                console.log("ID reemplazado");
+            } else{
+                this.byId = { error : 'producto no encontrado' };
+                console.log("No existe elemento con esa ID");
             }
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            let datos = await fs.promises.readFile(this.nombreArchivo,"utf-8");
+            let datosJson = await JSON.parse(datos);
+            console.log(datosJson)
+            if (datosJson.find(i=>i.id == id)){
+                let borrarId = datosJson.find(i=>i.id == id);
+                let posicionId = datosJson.indexOf(borrarId);
+                datosJson.splice(posicionId,1);
+                this.datos = datosJson;
+                fs.promises.writeFile(this.nombreArchivo, JSON.stringify(datosJson, null, 1));
+                console.log("ID eliminado");
+            } else{
+                this.datos = { error : 'producto no encontrado' };
+                console.log("No existe elemento con esa ID");
+            }
+        } catch (error) {
+            console.log(error);
         }
     }
 };
@@ -85,7 +129,15 @@ router.get("/:id", (req,res) => {
     producto.getById(id).then(() => res.json(producto.byId));
 })
 router.post('/', (req, res) => {
-    const { body } = req;
-    console.log(body)
-    producto.save(body).then(() => res.json(producto.datos));
-});  
+    const body = req.body;
+    producto.save(body).then(() => res.json(producto.byId));
+});
+router.put("/:id", (req,res) => {
+    const { id } = req.params;
+    const body = req.body;
+    producto.updateById(id,body).then(() => res.json(producto.byId));
+})
+router.delete("/:id", (req,res) => {
+    const { id } = req.params;
+    producto.deleteById(id).then(() => res.json(producto.datos));
+})  
--- READme.md
+++ READme.md
@@ -1,13 +1,21 @@
 # **Curso Backend**
-# Entrega 3
+# Entrega 4
 ## Camilo González
 ## Tutor: Raúl Ahumada
 #
 >Hola!
+
+>Esta es la entrega 4.
+
+>Para los metodos GET, se puede acceder los productos en la direccion http://localhost:8080/api/productos/ y http://localhost:8080/api/productos/ID, siendo ID el numero del item. En este caso, del 1 al 5, O mas despues del POST.
 >
->Gracias por la correción de la semana pasada.\
->Favor visitar las rutas /productos y /productorandom.
+>El index con el formulario, permite ingresar un producto y sus caracteristicas, Al hacer click en Enviar, se envia un POST que devuelve el nuevo objecto con la id entregada por el backend.
 >
+>El PUT devuelve el objeto con sus propiedades actualizadas.
+>
+>El DELETE devuelve el listado para mostrar que el producto solicitado por ID en la direccion, ha sido borrado.
+
+
 >Saludos
 
 
