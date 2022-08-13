529f3115fb593dfe673546327ce6ebafca7f23a8
cmlgnzlz <milogonzm@gmail.com>
2022-08-12 22:25:50 -0400

    Entrega 5 EJS

--- READme.md
+++ READme.md
@@ -1,1 +1,12 @@
-hola
+# **Curso Backend**
+# Entrega 5
+## EJS
+#
+## Camilo González
+## Tutor: Raúl Ahumada
+#
+>Hola!\
+>Esta es la entrega 5, parte EJS.
+>
+>Saludos
+#
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -13,13 +14,58 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 
-let productsHC = [
-  { id: 1, title: 'nike ball', price: 101, thumbnail: 'http://localhost:8080/public/nike-ball.jpg' },
-  { id: 2, title: 'nike shoes', price: 102, thumbnail: 'http://localhost:8080/public/nike-shoes.jpg' },
-  { id: 3, title: 'adidas shoes', price: 102, thumbnail: 'http://localhost:8080/public/adidas-shoes.jpg' },
-];
+class Contenedor{
 
-app.get('/products', (req, res) => {
-  console.log('aqui products');
-  res.render('pages/index', { title: 'listado de productos', products: productsHC });
+  constructor(nombreArchivo) {
+      this.nombreArchivo = nombreArchivo;
+      this.datos = [];
+      this.byId = {};
+  }
+
+  async getAll(){
+      try {
+          let datos = await fs.promises.readFile(this.nombreArchivo,"utf-8");
+          let datosJson = await JSON.parse(datos);
+          this.datos = datosJson;
+      } catch (error) {
+          console.log(error);
+      }
+  }
+
+  async save(product) {
+      try {
+          let datos = await fs.promises.readFile(this.nombreArchivo,"utf-8");
+          let datosJson = await JSON.parse(datos);
+          if (datosJson.length>0){
+              let maxId = datosJson.map(i=>i.id).sort((a, b) => {if(a == b) {return 0;}if(a < b) {return -1;}return 1;}).splice(-1);
+              let nuevoId = parseInt(maxId)+1;
+              product = {id:nuevoId, ...product}
+              datosJson.push(product);
+              this.byId = product;
+              this.datos = datosJson;
+              fs.promises.writeFile(this.nombreArchivo, JSON.stringify(datosJson, null, 1));
+          }
+          else{
+              this.datos = { error : 'producto no encontrado' };
+          }
+      } catch (error) {
+          console.log(error);
+      }
+  }
+};
+
+const producto= new Contenedor("productos.txt");
+
+app.get("/", (req,res) => {  
+    res.render('pages/formulario', {title: 'Formulario'});
 });
+
+app.get('/productos', (req, res) => {
+  const body = req.body;
+  producto.getAll(body).then(() => res.render('pages/productos', { productos: producto.datos, title: 'Listado de productos' }));
+})
+
+app.post('/productos', (req, res) => {
+    const body = req.body;
+    producto.save(body).then(() => res.render('pages/productos', { productos: producto.datos, title: 'Listado de productos' }));
+})
